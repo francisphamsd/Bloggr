4ce3dbb8a442227347c41b75917fda1f984cb25b
francisphamsd <fphamsd@gmail.com>
2022-01-24 18:32:02 -0800

    Initialize

---
+++ Bloggr/Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿namespace Bloggr.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdatedDate { get; set;} = DateTime.Now;
+    }
+}
